4589fc77d00daa347336a7827c56076d5e06cf21
Gerardo Gonzalez <ggonza14@ut.utm.edu>
2024-03-14 05:13:51 -0500

    First commit

--- library.cpp
+++ library.cpp
@@ -6,3 +6,53 @@
 #include <string>
 
 using namespace std;
+
+library::library() {
+  list<movie> movies;
+}
+
+void library::read_from_file(const string file_name) {
+  ifstream inFile;
+  inFile.open(file_name);
+  if(!inFile) {
+    cout << "There was an error opening the file, try again" << endl;
+  }
+  string Title;
+  string DirectorName;
+  int Runtime;
+  string Format;
+  float Price;
+  int Year;
+  getline(inFile, Title);
+  while(inFile) {
+    getline(inFile, DirectorName);
+    inFile >> Runtime;
+    inFile >> Format;
+    inFile >> Price;
+    inFile >> Year;
+    insert_sorted(Title, DirectorName, Runtime, Format, Price, Year);
+    getline(inFile, Title);
+  }
+  inFile.close();
+}
+
+void library::insert_sorted(const string title, const string directorName, const int runtime, const string format, const float price, const int year) {
+  movie insert;
+  list<movie>::iterator it = movies.begin();
+  insert.Title = title;
+  insert.DirectorName = directorName;
+  insert.Runtime = runtime;
+  insert.Format = format;
+  insert.Price = price;
+  insert.Year = year;
+  if(movies.begin().Title > insert.Title) {
+    movies.push_front(insert);
+  } else {
+    while(it.Title < insert.Title) {
+      it++;
+    }
+    movies.insert(it, insert);
+  }
+  
+}
+  
--- library.h
+++ library.h
@@ -2,7 +2,6 @@
 #define LIBRARY_H
 #include <string>
 
-
 class library {
  private:
   struct movie {
@@ -13,10 +12,11 @@ class library {
     float Price;
     int Year; //don't know if to make the type an int or short, just going with int for now.
   };
+  
  public:
   library();
   ~library();
-  void insert_sorted();
+  void insert_sorted(const std::string title, const std::string directorName, const int runtime, const std::string format, const float price, const int year);
   void read_from_file(const std::string file_name);
   void write_from_file(const std::string file_name);
   void print();
